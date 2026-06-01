9e8139e4ea98bf2236765997044ffc3baefded6b
Momster <bigbob12352@outlook.com>
2026-06-01 17:37:24 +0800

    Create FindReplaceDialog.h

---
+++ histories/src3_20260601/FindReplaceDialog.h
@@ -0,0 +1,33 @@
+﻿#ifndef FINDREPLACEDIALOG_H
+#define FINDREPLACEDIALOG_H
+
+#include <QDialog>
+#include <QPlainTextEdit>
+
+class QLineEdit;
+class QCheckBox;
+class QPushButton;
+
+class FindReplaceDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit FindReplaceDialog(QPlainTextEdit *editor, QWidget *parent = nullptr);
+
+private slots:
+    void findNext();
+    void replace();
+    void replaceAll();
+
+private:
+    QPlainTextEdit *m_editor;
+    QLineEdit *m_findEdit;
+    QLineEdit *m_replaceEdit;
+    QCheckBox *m_caseSensitive;
+    QPushButton *m_findButton;
+    QPushButton *m_replaceButton;
+    QPushButton *m_replaceAllButton;
+};
+
+#endif // FINDREPLACEDIALOG_H
