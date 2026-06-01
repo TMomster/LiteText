6be6988b89cea421dc44276470d2c71be8a80b54
Momster <bigbob12352@outlook.com>
2026-06-01 17:37:32 +0800

    Create SettingsDialog.h

---
+++ histories/src3_20260601/SettingsDialog.h
@@ -0,0 +1,51 @@
+﻿#ifndef SETTINGSDIALOG_H
+#define SETTINGSDIALOG_H
+
+#include <QDialog>
+#include <QFont>
+
+class QComboBox;
+class QSpinBox;
+class QPushButton;
+class QCheckBox;
+
+class SettingsDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit SettingsDialog(QWidget *parent = nullptr);
+    ~SettingsDialog();
+
+    QFont getSelectedFont() const;
+    int getFontSize() const;
+    int getTabWidth() const;
+    bool getUseTabs() const;
+    bool isWordWrapEnabled() const;
+    bool isAutoCompletionEnabled() const;
+    int getCompletionAcceptKey() const;
+    int getColorScheme() const;
+    QString getDefaultEncoding() const;
+
+private slots:
+    void accept() override;
+
+private:
+    QComboBox *m_fontCombo;
+    QSpinBox *m_fontSizeSpin;
+    QComboBox *m_colorSchemeCombo;
+    QSpinBox *m_tabWidthSpin;
+    QCheckBox *m_useTabsCheck;
+    QCheckBox *m_wordWrapCheck;          // 自动换行
+    QCheckBox *m_autoCompletionCheck;
+    QComboBox *m_acceptKeyCombo;
+    QComboBox *m_encodingCombo;          // 文件编码
+    QPushButton *m_okButton;
+    QPushButton *m_cancelButton;
+
+    void loadSettings();
+    void saveSettings();
+    QString configFilePath() const;
+};
+
+#endif // SETTINGSDIALOG_H
